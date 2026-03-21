76d319a3e25a394e44d55df7b8096c9375daccb6
chakri <25b1070@iitb.ac.in>
2026-03-21 23:18:03 +0530

    hash is left in bash interfernce

---
+++ main.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+echo Username1 : 
+read User1
+grep -E "^\${User1}$" users.tsv > line1.txt
+if [[ $(cat line1.txt) == "" ]];then
+	echo "This Username name doesn't exist do you wanna register"
+	rm line1.txt
+else 	
+	echo Password1 :
+	read Pass1
+	Hash1=hash(Pass1)
+	if [[ $Hash1 != $(cut -f 2 line1.txt ) ]];then
+	       echo "This Password doesn't match Try again"
+	       rm line1.txt
+        else	
+	 	rm line1.txt	
+		echo Username2 :
+		read User2
+		grep -E "^\${User2}$" users.tsv > line2.txt
+		if [[ $(cat line2.txt) == "" ]];then
+		       	echo "This Username name doesn't exist do you wanna register"
+			rm line2.txt 
+	       	else		
+			echo Pass2 :
+			read Pass2
+			Hash2=hash(Pass2)
+        		if [[ $Hash2 != $(cut -f 2 line2.txt ) ]];then
+               			echo "This Password doesn't match Try again"
+				rm line2.txt
+			else
+				rm line2.txt
+				python3 game.py $User1 $User2
+			fi
+		fi
+	fi
+fi
