6705343c457b2be6c180645fdb649939f5433e91
Bipul Kumar <bipulkumar27av@gmail.com>
2026-04-01 20:10:34 +0530

    add classes in game.py

--- game.py
+++ game.py
@@ -1,44 +1,39 @@
-import pygame 
+import pygame
 import numpy as np 
 import matplotlib
 import pathlib
 import sys
 import os 
 import time
+import abc
 
+USER1 = sys.argv[1] 
+USER2 = sys.argv[2]
 
-username1 = sys.argv[1] 
-username2 = sys.argv[2]
+TURN = 0
 
-
-def Choose_Character():
-    pass
-
-pygame.init()
-screen=pygame.display.set_mode((1280,720))
-clock = pygame.time.Clock()
-running=True
-
-i=1
-a=1
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running=False
-
-
-    screen.fill((i%256,0,(255-i)%256))
+class Player:
+    def __init__(self, user_name, turn):
+        self.user_name = user_name
+        self.turn = TURN
     
+class Board:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.board = np.zeros((width , height))
+
+class Game():
+    def __init__(self, player1, player2):
+        self.player1 = player1
+        self.player2 = player2
+
+    def switch_turn(self):
+        self.player1.turn = 1 - self.player1.turn
+        self.player2.turn = 1 - self.player2.turn
+
+    @abc.abstractmethod
+    def check_win(self):
+        """ This would be used to check win condition """
+        return
     
-    if  i==255 :
-        a=-1
-    elif i==0: 
-        a=1
-    i+=a
-
-
-    pygame.display.flip()
-
-    clock.tick(60)
-
-pygame.quit()
