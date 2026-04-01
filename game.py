a14838fa804b23c713a5aba3c350c0d940d40826
Bipul Kumar <bipulkumar27av@gmail.com>
2026-04-01 22:54:33 +0530

    add button functionality

--- game.py
+++ game.py
@@ -11,6 +11,11 @@ USER2 = sys.argv[2]
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 960
 TURN = 0
+SIZE = SCREEN_WIDTH, SCREEN_HEIGHT
+BUTTON_SIZE = (SCREEN_WIDTH/3, 10)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BGCOLOR = (39, 135, 245)
 
 class Player:
     def __init__(self, user_name, turn):
@@ -37,12 +42,41 @@ class Game:
         return
     
 # handle gameplay
+pygame.init()
+
+HEADER_FONT = pygame.font.SysFont("Papyrus", 60)
+BUTTON_FONT = pygame.font.SysFont("Calibri", 25)
+
 player1 = Player(USER1, 1)
 player2 = Player(USER2, 0)
 
-pygame.init()
+WINDOW = pygame.display.set_mode(SIZE)
 
-SIZE = SCREEN_WIDTH, SCREEN_HEIGHT
-screen = pygame.display.set_mode(SIZE)
+def make_button(text, center_x, center_y):
+    pass
 
+def start_menu():
+    pygame.display.set_caption("GAME HUB")
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                sys.exit()
+
+        WINDOW.fill(BGCOLOR)
+
+        HEADER_TEXT = HEADER_FONT.render(f"{USER1} V/S {USER2}", True, WHITE)
+        WINDOW.blit(HEADER_TEXT, HEADER_TEXT.get_rect(center = (SCREEN_WIDTH/2, SCREEN_HEIGHT/5)))
+
+        TICTAC_BG_RECT = pygame.Rect(0, 0, SCREEN_WIDTH/4, 50)
+        TICTAC_BG_RECT.center = (SCREEN_WIDTH/2, 0.4 * SCREEN_HEIGHT)
+
+        pygame.draw.rect(WINDOW, WHITE, TICTAC_BG_RECT)
+        TICTAC_BUTTON_TEXT = BUTTON_FONT.render("TIC-TAC-TOE", True, BLACK)
+        TICTAC_RECT = TICTAC_BUTTON_TEXT.get_rect(center = (SCREEN_WIDTH/2, 0.4 * SCREEN_HEIGHT))
+        WINDOW.blit(TICTAC_BUTTON_TEXT, TICTAC_RECT)
+        pygame.display.update()
+
+start_menu()
 
