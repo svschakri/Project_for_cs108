3110eb5d7911b13fb5d71fcb4b3cdc28f1f83afe
Bipul Kumar <bipulkumar27av@gmail.com>
2026-04-16 16:58:48 +0530

    Add game over screen to game.py

--- game.py
+++ game.py
@@ -9,6 +9,64 @@ import pygame
 import time
 import subprocess
 
+# dimensions
+SCREEN_WIDTH = 1920
+SCREEN_HEIGHT = 1000
+screen_size = SCREEN_WIDTH, SCREEN_HEIGHT
+
+title_ht, title_wt = SCREEN_HEIGHT // 5, SCREEN_WIDTH
+title_header_gap = SCREEN_HEIGHT // 20
+
+header_ht = SCREEN_HEIGHT // 6
+header_wt = 2 * (SCREEN_WIDTH // 3)
+HEADER_BORDER_RADIUS = 50
+
+GAME_PATH = {} 
+GAME_LIST = []
+
+with open("games.csv", "r") as f:
+    for line in f:
+        line_arr = line.strip().split(",")
+        GAME_PATH[line_arr[0]] = line_arr[1]
+        GAME_LIST.append(line_arr[0])
+
+GAME_LIST = list(GAME_PATH.keys())
+
+button_number = len(GAME_LIST)
+button_wt = SCREEN_WIDTH // 3         
+button_ht = SCREEN_HEIGHT // 12
+button_gap = SCREEN_HEIGHT // 24
+button_stack_ht = (button_number)*button_gap + (button_number+1)*button_ht # include quit button
+
+header_box_gap = (SCREEN_HEIGHT - title_ht - title_header_gap - header_ht - button_stack_ht) // 3
+
+# colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BGCOLOR = (245, 182, 66)
+
+TITLE_BG = (111, 245, 66)
+TITLE_COLOR = WHITE
+
+HEADER_BG = (239, 245, 66)
+HEADER_COLOR = BLACK
+
+BUTTON_BG = (245, 66, 233)
+LIGHT_BUTTON_BG = (237, 147, 227)
+BUTTON_FONT_COLOR = WHITE
+
+QUIT_BG = (242, 90, 63)
+LIGHT_QUIT_BG = (242, 121, 99)
+TITLE_FONT_COLOR = (255, 255, 255)
+
+# Game over screen constants
+over_title_wt = 2* (SCREEN_WIDTH // 3)
+over_title_ht = SCREEN_HEIGHT // 7
+over_title_y = SCREEN_HEIGHT // 4
+title_button_gap = over_title_ht
+OVER_TITLE_BG = (85, 250, 148)
+BOX_BORDER_RADIUS = 15
+
 class Player:
     def __init__(self, user_name):
         self.user_name = user_name
@@ -32,61 +90,90 @@ class Game:
     def check_win(self, screen = None):
         """ This would be used to check win condition """
         return
+    
+    def make_title(self, screen, title_font, text_str, wt = SCREEN_WIDTH, ht = over_title_ht, center_y = over_title_ht // 2):
+        bg_rect = pygame.Rect(0, 0, wt, ht)
+        bg_rect.center = (SCREEN_WIDTH // 2, center_y)
+
+        pygame.draw.rect(screen, OVER_TITLE_BG, bg_rect)
+        text = title_font.render(text_str, True, TITLE_FONT_COLOR)
+        text_rect = text.get_rect(center = bg_rect.center)
+        screen.blit(text, text_rect)
+
+    def make_rect(self, center_y):   
+        button_rect = pygame.Rect(0, 0, button_wt, button_ht)
+        button_rect.center = (SCREEN_WIDTH // 2, center_y)
+        return button_rect
+
+    def make_button(self, screen, button_font, button_rect, text_str, mouse):
+        button_color = BUTTON_BG if text_str != "Quit" else QUIT_BG
+        if button_rect.collidepoint(mouse):
+            button_color = LIGHT_BUTTON_BG if text_str != "Quit" else LIGHT_QUIT_BG
+
+        text = button_font.render(text_str, True, BUTTON_FONT_COLOR)
+        pygame.draw.rect(screen, button_color, button_rect, border_radius=BOX_BORDER_RADIUS)
+        text_rect = text.get_rect(center = button_rect.center)
+        screen.blit(text, text_rect)
+        return button_rect
+
+    def draw_game_over(self, screen, title_font, msg):
+
+        if not pygame.get_init():
+            pygame.init()
+
+        command = 0
+
+        running = True
+        button_font = pygame.font.SysFont("calibri", 40)
+        while(running):
+            mouse = pygame.mouse.get_pos()
+            screen.fill(BGCOLOR)
+            self.make_title(screen, title_font, msg, over_title_wt, over_title_ht, over_title_y)
+            rect_dict = {}
+            first_button_y = over_title_y + over_title_ht // 2 + title_button_gap + button_ht // 2
+            rect_dict["Play Again"] = self.make_rect(first_button_y)
+            rect_dict["Show Leaderboard"] = self.make_rect(first_button_y + button_ht + button_gap)
+            quit_rect = self.make_rect(first_button_y + 2*(button_ht + button_gap))
+
+            for name, rect in rect_dict.items():
+                self.make_button(screen, button_font, rect, name, mouse)
+            self.make_button(screen, button_font, quit_rect, "Quit", mouse)
+            
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                        command = 0
+                        running = False
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if quit_rect.collidepoint(mouse):
+                        command = 0
+                        running = False
+                    
+                    for name, rect in rect_dict.items():
+                        if rect.collidepoint(mouse):
+                            command = 1 if name == "Play Again" else 2
+                            running = False
+            pygame.display.flip()
+        pygame.quit()
+        return command
+
+    def check_result(self, screen, title_font, game, win):
+        user1 = game.player1.user_name
+        user2 = game.player2.user_name
+        msgDict = {1 : f"{user1} WON!", 2 : f"{user2} WON!", 0 : "DRAW!"}
+
+        if win == 1 or win == 2 or win == 0:
+            self.make_title(screen, title_font, msgDict[win])
+            pygame.display.flip()
+            pygame.time.wait(1000)
+            return self.draw_game_over(screen, title_font, msgDict[win])
+        
+        return -1
 
 if __name__ == "__main__":
     # handle users
     user1 = sys.argv[1] 
     user2 = sys.argv[2]
 
-    # dimensions
-    SCREEN_WIDTH = 1920
-    SCREEN_HEIGHT = 1000
-    screen_size = SCREEN_WIDTH, SCREEN_HEIGHT
-
-    title_ht, title_wt = SCREEN_HEIGHT // 5, SCREEN_WIDTH
-    title_header_gap = SCREEN_HEIGHT // 20
-
-    header_ht = SCREEN_HEIGHT // 6
-    header_wt = 2 * (SCREEN_WIDTH // 3)
-    HEADER_BORDER_RADIUS = 50
-
-    GAME_PATH = {} 
-    GAME_LIST = []
-
-    with open("games.csv", "r") as f:
-        for line in f:
-            line_arr = line.strip().split(",")
-            GAME_PATH[line_arr[0]] = line_arr[1]
-            GAME_LIST.append(line_arr[0])
-
-    GAME_LIST = list(GAME_PATH.keys())
-
-    button_number = len(GAME_LIST)
-    button_wt = SCREEN_WIDTH // 3         
-    button_ht = SCREEN_HEIGHT // 12
-    button_gap = SCREEN_HEIGHT // 24
-    button_stack_ht = (button_number)*button_gap + (button_number+1)*button_ht # include quit button
-
-    header_box_gap = (SCREEN_HEIGHT - title_ht - title_header_gap - header_ht - button_stack_ht) // 3
-    BOX_BORDER_RADIUS = 10
-
-    # colors
-    WHITE = (255, 255, 255)
-    BLACK = (0, 0, 0)
-    BGCOLOR = (245, 182, 66)
-
-    TITLE_BG = (111, 245, 66)
-    TITLE_COLOR = WHITE
-
-    HEADER_BG = (239, 245, 66)
-    HEADER_COLOR = BLACK
-
-    BUTTON_BG = (245, 66, 233)
-    LIGHT_BUTTON_BG = (237, 147, 227)
-    BUTTON_FONT_COLOR = WHITE
-
-    QUIT_BG = (242, 90, 63)
-    LIGHT_QUIT_BG = (242, 121, 99)
         
     # menu design
     def init_pygame():
@@ -107,7 +194,7 @@ if __name__ == "__main__":
         text_rect = text.get_rect(center = bg_rect.center)
         screen.blit(text, text_rect)
 
-    def make_button(screen, button_font, text_str, center_y, mouse):
+    def make_button_icon(screen, button_font, text_str, center_y, mouse):
         text = button_font.render(text_str, True, BUTTON_FONT_COLOR)
         button_rect = pygame.Rect(0, 0, button_wt, button_ht)
         button_rect.center = (SCREEN_WIDTH // 2, center_y)
@@ -122,7 +209,7 @@ if __name__ == "__main__":
         return button_rect
 
     def play_game(game_name):
-        module_path = GAME_PATH[game_name]   # e.g. "games.tictactoe"
+        module_path = GAME_PATH[game_name]
 
         game_module = __import__(module_path, fromlist=['run'])
 
@@ -179,11 +266,11 @@ if __name__ == "__main__":
             for i in range(button_number):
                 game_name = GAME_LIST[i]
                 center_i = buttons_top + i * (button_ht + button_gap) + button_ht // 2
-                game_rect_list.append((game_name, make_button(screen, button_font, game_name, center_i, mouse)))
+                game_rect_list.append((game_name, make_button_icon(screen, button_font, game_name, center_i, mouse)))
             
             # quit button
             quit_center = buttons_top + (button_number) * (button_ht + button_gap) + button_ht // 2
-            quit_rect = make_button(screen, button_font, "Quit", quit_center, mouse)
+            quit_rect = make_button_icon(screen, button_font, "Quit", quit_center, mouse)
             
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
--- games/connect4.py
+++ games/connect4.py
@@ -49,7 +49,7 @@ title_button_gap = over_title_ht
 button_wt = SCREEN_WIDTH // 3    
 button_ht = SCREEN_HEIGHT // 12
 button_gap = SCREEN_HEIGHT // 24
-BOX_BORDER_RADIUS = 10
+BOARD_BORDER_RADIUS = 30
 
 BGCOLOR = (245, 182, 66)
 BUTTON_BG = (245, 66, 233)
@@ -141,7 +141,7 @@ def make_board(screen, board_matrix, mouse):
     board_rect = pygame.Rect(0, 0, board_wt, board_ht)
     board_rect.center = (SCREEN_WIDTH // 2, center_y)
 
-    pygame.draw.rect(screen, BOARD_COLOR, board_rect, 0, 10, 10)
+    pygame.draw.rect(screen, BOARD_COLOR, board_rect, 0, BOARD_BORDER_RADIUS, BOARD_BORDER_RADIUS)
     for i in range(0, COLS):
         for j in range(0, ROWS):
             make_board_circle(screen, i+1, j+1, board_matrix[i][j])
@@ -151,77 +151,6 @@ def make_board(screen, board_matrix, mouse):
             for j in range(ROWS):
                 if board_matrix[i][j] == 0:
                     make_board_circle(screen, i+1, j+1, 3)
-
-def make_rect(center_y):   
-        button_rect = pygame.Rect(0, 0, button_wt, button_ht)
-        button_rect.center = (SCREEN_WIDTH // 2, center_y)
-        return button_rect
-
-def make_button(screen, button_font, button_rect, text_str, mouse):
-        button_color = BUTTON_BG if text_str != "Quit" else QUIT_BG
-        if button_rect.collidepoint(mouse):
-            button_color = LIGHT_BUTTON_BG if text_str != "Quit" else LIGHT_QUIT_BG
-
-        text = button_font.render(text_str, True, BUTTON_FONT_COLOR)
-        pygame.draw.rect(screen, button_color, button_rect, border_radius=BOX_BORDER_RADIUS)
-        text_rect = text.get_rect(center = button_rect.center)
-        screen.blit(text, text_rect)
-        return button_rect
-
-def draw_game_over(screen, title_font, msg):
-
-    if not pygame.get_init():
-        pygame.init()
-
-    command = 0
-
-    running = True
-    button_font = pygame.font.SysFont("calibri", 40)
-    while(running):
-        mouse = pygame.mouse.get_pos()
-        screen.fill(BGCOLOR)
-        make_title(screen, title_font, msg, over_title_wt, over_title_ht, over_title_y)
-        rect_dict = {}
-        first_button_y = over_title_y + over_title_ht // 2 + title_button_gap + button_ht // 2
-        rect_dict["Play Again"] = make_rect(first_button_y)
-        rect_dict["Show Leaderboard"] = make_rect(first_button_y + button_ht + button_gap)
-        quit_rect = make_rect(first_button_y + 2*(button_ht + button_gap))
-
-        for name, rect in rect_dict.items():
-            make_button(screen, button_font, rect, name, mouse)
-        make_button(screen, button_font, quit_rect, "Quit", mouse)
-        
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                    command = 0
-                    running = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if quit_rect.collidepoint(mouse):
-                    command = 0
-                    running = False
-                
-                for name, rect in rect_dict.items():
-                    if rect.collidepoint(mouse):
-                        command = 1 if name == "Play Again" else 2
-                        running = False
-        pygame.display.flip()
-    pygame.quit()
-    return command
-    
-
-def check_result(screen, title_font, game):
-    user1 = game.player1.user_name
-    user2 = game.player2.user_name
-    msgDict = {1 : f"{user1} WON!", 2 : f"{user2} WON!", 0 : "DRAW!"}
-    win = game.check_win()
-
-    if win == 1 or win == 2 or win == 0:
-        make_title(screen, title_font, msgDict[win])
-        pygame.display.flip()
-        pygame.time.wait(1000)
-        return draw_game_over(screen, title_font, msgDict[win])
-    
-    return -1
 
 def run(user1, user2):
     INIT_TURN = 1
@@ -260,7 +189,8 @@ def run(user1, user2):
         make_title(screen, title_font, title_text)
         make_board(screen, board_matrix, mouse)
 
-        command = check_result(screen, title_font, game)
+        win = game.check_win()
+        command = game.check_result(screen, title_font, game, win)
         if command != -1:
             return command
 
